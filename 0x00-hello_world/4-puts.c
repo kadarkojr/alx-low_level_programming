f9ede6010139fc8f3e6ca4f18d3f3a0121b6edd9
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-07-13 12:45:24 -0700

    4th biom

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /*
-* main - a program that prints text with puts function
-* return: 0 (Success)
-*/
-
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
