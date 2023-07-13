97cdea6b3abfc3aad2acffc8aa255152ae42a52e
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-07-13 16:08:36 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
+int main()
+{
+    fprintf(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+}
