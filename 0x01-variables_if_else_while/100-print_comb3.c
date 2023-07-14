de07c1fd70a2b5dfc0ca10f581207b32241f15e0
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-07-14 15:41:14 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+int j;
+
+for (i = 0; i < 10; i++)
+{
+	for(j = i; j < 10; j++)
+	{
+	if ( i == j)
+	{
+	i = i;
+	j = j;
+	}
+	else
+	{
+	putchar('0' + i);
+	putchar('0' + j);
+	if (i == 8 && j == 9)
+	{
+	i = i;
+	j = j;
+	}
+	else
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+}
+}
