df91d89852cd9866fe7cfef7140758919e172622
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-07-14 13:13:33 -0700

    5th

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+
+for (i = 97; i < 123; i++)
+{
+if(i == 101 || i == 113)
+{
+i = i;
+}
+else{
+putchar(i);
+}
+}
+putchar('\n');
+return (0);
+}
