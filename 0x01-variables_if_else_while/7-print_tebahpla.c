2bae6d96d77e014e6e52d83836f0a0fc1f57cdd7
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-07-14 14:34:12 -0700

    8

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+
+for (i = 122; i > 96; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
