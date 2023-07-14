dd42afd3adafec473bcf6a9d6de155800f3a8c5e
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-07-14 11:51:38 -0700

    0th

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <time.h>
+/**
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    if( n > 0 ) {
+        printf("%d is positive", n);
+    } else if( n < 0) {
+        printf("%d is negative", n);
+    } else{
+        printf("%d is zero", n);
+    }
+}
