60794d3a78928c6b1cb73e55a687b4ab36b600a7
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-07-14 15:10:07 -0700

    10 3

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,9 +13,11 @@ int i;
 for (i = 0; i < 10; i++)
 {
 putchar('0' + i);
-	if (i != 9)
-		putchar(',');
-		putchat(' ');
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
 }
 putchar('\n');
 return (0);
