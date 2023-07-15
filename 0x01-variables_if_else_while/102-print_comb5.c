d3b99124b7533baf66c9f5d18abbe9b6809e5cab
kadarkojr <136028741+kadarkojr@users.noreply.github.com>
2023-07-15 08:52:10 +0000

    102 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,15 +18,15 @@ putchar('0' + (i % 10));
 putchar(' ');
 putchar('0' + (j / 10));
 putchar('0' + (j % 10));
-    if (i == 98 && j == 99)
-    {
-    continue;
-    }
-    else
-    {
-    putchar(',');
-    putchar(' ');
-    }
+if (i == 98 && j == 99)
+{
+continue;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
 }
 }
 putchar('\n');
