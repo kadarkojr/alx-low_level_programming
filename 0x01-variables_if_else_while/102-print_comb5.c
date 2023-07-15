db6429b809e05c03e856607662fc2b74ae3521cc
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-07-15 01:09:48 -0700

    102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
+int main() {
+int i; 
+int j;
+
+int l = 10;
+for (i = 0; i < 100; i++) {
+for (j = i; j < 100; j++) {
+putchar('0' + (i / 10));
+putchar('0' + (i % 10));
+putchar(' ');
+putchar('0' + (j / 10));
+putchar('0' + (j % 10));
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return 0;
+}
