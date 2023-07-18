c0ce37afd6b4aeed0e6ba2bf50e5d62230e670cc
kadarkojr <136028741+kadarkojr@users.noreply.github.com>
2023-07-18 20:46:47 +0000

    6th 9

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,17 +7,17 @@
 */
 int print_sign(int c)
 {
-    if (c > 0)
+if (c > 0)
 {
 _putchar('+');
 return (1);
 }
-    else if (c == 0)
+else if (c == 0)
 {
 _putchar(0 + '0');
 return (0);
 }
-    else if (c < 0)
+else if (c < 0)
 {
 _putchar('-');
 }
