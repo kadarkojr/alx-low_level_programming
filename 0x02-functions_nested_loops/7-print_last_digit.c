5a00cad1ecd7c746c1ceb6e893093e466b3e8381
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-07-18 14:21:59 -0700

    8th

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_last_digit - return the last digit of a number
+* @c: check character c
+* Return: last value
+*/
+int print_last_digit(int c)
+{
+int a;
+a = c % 10;
+if (a < 0)
+{
+a = -a;
+}
+else
+{
+a = a;
+}
+printf("%d", a);
+return (a);
+}
