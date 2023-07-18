0cb47f579a1b7e7c56f2ab6bca1128fd360e8b27
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-07-18 13:28:02 -0700

    6th 3

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,13 +10,16 @@ int print_sign(int c)
 if (c > 0)
 {
     putchar('+');
+    return (1);
 }
 else if (c == 0)
 {
     putchar(0 + '0');
+    return (0);
 }
 else
 {
     putchar('-');
+    return (-1);
 }
 }
