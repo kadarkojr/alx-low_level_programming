a5b0606651ad14dccd7040e44f5bfb5192a27363
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-07-18 12:10:12 -0700

    2nd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+
+for (i = 97; i < 123; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
