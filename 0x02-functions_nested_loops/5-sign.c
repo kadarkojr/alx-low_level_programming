5424d5b63d9b868390a96ec7c6e877c41b990dfa
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-07-18 13:19:10 -0700

    6th

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//#include "main.h"
+/**
+* print_sign - check if an alphabet is in lowercase
+* @c: check character c
+* Print: + if positive, 0 if zero or - if negative
+*/
+int print_sign(int c)
+{
+if (c > 0)
+{
+    putchar('+');
+}
+else if (c == 0)
+{
+    putchar(0 + '0');
+}
+else
+{
+    putchar('-');
+}
+}
