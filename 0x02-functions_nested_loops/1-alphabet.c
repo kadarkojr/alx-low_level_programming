c7790145c6a5534656e69eb9e60dbf8c795d642d
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-07-18 12:18:09 -0700

    2nd 3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,7 @@
 *
 * Return: Always 0 (Success)
 */
-int main(void)
+void printalphabet(void)
 {
 int i;
 
@@ -14,5 +14,4 @@ for (i = 97; i < 123; i++)
 putchar(i);
 }
 putchar('\n');
-return (0);
 }
