6f745c454f29666366a05d55722133b8861daf4e
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-07-20 12:07:18 -0700

    2 3

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,12 +2,12 @@
 #include "main.h"
 
 /**
-* _mul - multiplies two digits
+* mul - multiplies two digits
 * @a: int to be multiplied by int b
 * @b: int to be multiplied by int a
 * Return: the product of a and b
 */
-int _mul(int a, int b)
+int mul(int a, int b)
 {
 int c;
 c = a * b;
