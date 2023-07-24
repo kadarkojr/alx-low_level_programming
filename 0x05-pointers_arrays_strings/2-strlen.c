1128b635922405bb94b9a15db45601cd5c04f3d5
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-07-24 12:43:30 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _strlen - print the length of a string
+* @s: char whose length is being checked
+*/
+
+int _strlen(char *s)
+{
+int len;
+len = strlen(s);
+return (len);
+}
