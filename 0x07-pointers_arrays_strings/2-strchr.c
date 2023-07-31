7411ea650221f7def6739fa18543b19cb1c5c070
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-07-31 14:13:56 -0700

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _strchr - return a pointer to the first occurence of c
+* in the string, or null if empty.
+* @*s: string
+* @c: char
+* Return: pointer to remainder of string
+*/
+char *_strchr(char *s, char c)
+{
+if(*s == '\0')
+{
+return (s);
+}
+else if(c == '\0')
+{
+return (s);
+}
+else
+{
+while(*s)
+{
+if(*s == c)
+{
+return (s);
+}
+s++;
+}
+}
+return ('\0');
+}
