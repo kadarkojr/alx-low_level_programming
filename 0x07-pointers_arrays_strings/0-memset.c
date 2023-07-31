852320fd8c40e4785d9fc28c8969d5a1a86c0b66
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-07-31 13:39:04 -0700

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,7 +6,7 @@
 * _memset - function to fill memory with a constant type)
 * @s: first char
 * @b: second char
-* @n: 
+* @n: integer
 * Return: s
 */
 char *_memset(char *s, char b, unsigned int n)
