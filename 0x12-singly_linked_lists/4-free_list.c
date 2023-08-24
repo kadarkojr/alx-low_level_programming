d15b92f2c41ed8796e4aa31df536dca059325e9b
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-08-24 11:59:28 -0700

    4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* free_list - free linked list
+* @head: linked list
+*/
+void free_list(list_t *head)
+{
+list_t *temp;
+
+while (head)
+{
+temp = head->next;
+free(head->str);
+free(head);
+head = temp;
+}
+}
