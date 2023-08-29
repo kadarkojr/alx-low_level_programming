7265e29345a8ba0219976813e747079487bc1217
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-08-29 11:32:30 -0700

    0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* print_listint - print linked list
+* @h: linked list
+* Return: nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+size_t num = 0;
+
+while (h)
+{
+printf("%d\n", h->n);
+num++;
+h = h->next;
+}
+return (num);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+stress all over again
