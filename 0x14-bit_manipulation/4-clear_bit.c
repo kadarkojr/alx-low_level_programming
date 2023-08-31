8557918442794675746ed9671e9386f5e6f1e635
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-08-31 14:20:48 -0700

    4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* clear_bit - set bit to 0
+* @n: pointer
+* @index: index
+* Return: 1 or -1
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (index > 63)
+return (-1);
+*n = (~(1UL << index) & *n);
+return (1);
+}
