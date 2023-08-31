299875e65b135a70f52c833948f45197174a6ebd
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-08-31 13:53:36 -0700

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_binary - print binary
+* @n: number
+*/
+
+void print_binary(unsigned long int n)
+{
+int i = 0;
+int count = 0;
+unsigned long int current;
+
+for (i = 63; i >= 0; i--)
+{
+current = n >> i;
+
+if (current & 1)
+{
+_putchar('1');
+count++;
+}
+else if (count)
+_putchar('0');
+}
+if (!count)
+_putchar('0');
+}
